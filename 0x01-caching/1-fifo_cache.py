7eb782e14517a73b3e6beeddeacd984ae9fdee16
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-26 16:26:57 +0000

    task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+FIFO: First In, First Out task.
+"""
+
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    A caching system class.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        This adds an item to the dict with a key and value.
+
+        args:
+            key -> the key of the item.
+            item -> the value stored in the key.
+        """
+        if key is None and item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            first_key = self.queue.pop(0)
+            del self.cache_data[first_key]
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """
+        This gets the key:value pair in the dict.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
