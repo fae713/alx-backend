fdccf26a984f573ce122a8b478ee33f5b59cc5af
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-26 15:58:38 +0000

    Task 1

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,6 +5,7 @@ Basic Dictionary task.
 
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """
     This class creates a basic dictionary.
@@ -13,13 +14,13 @@ class BasicCache(BaseCaching):
         """
         This adds an item to the dict with a key and value.
 
-        args: 
+        args:
             key -> the key of the item.
             item -> the value stored in the key.
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """
         This gets the key:value pair in the dict.
@@ -27,5 +28,3 @@ class BasicCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
-
-
