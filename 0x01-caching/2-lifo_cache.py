5ed7b219d20912fc3bf7410846fd06eb0706cfe7
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-26 16:51:31 +0000

    Task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+LIFO: Last In, First Out caching task.
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    A LIFO caching class.
+    """
+    def __init__(self):
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """
+        This adds an item to the dict with a key and value.
+
+        args:
+            key -> the key of the item.
+            item -> the value stored in the key.
+        """
+        if key is None and item is None:
+            return
+
+        if key in self.cache_data:
+            self.stack.remove(key)
+
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            last_key = self.stack.pop()
+            del self.cache_data[last_key]
+            print(f"DISCARD: {last_key}")
+
+        self.cache_data[key] = item
+        self.stack.append(key)
+
+    def get(self, key):
+        """
+        This gets the key:value pair in the dict.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
