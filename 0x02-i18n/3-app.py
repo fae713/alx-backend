5a35d33acebdae13e3b9cf50cdc2692b80abc255
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-07-02 13:33:46 +0000

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Parametrize templates.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config(object):
+    """
+    Config class.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "utc"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index() -> str:
+    """
+    This is the home route.
+    """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets the locale for a web page.
+
+    returns:
+            str = best match.
+    """
+    best_match = request.accept_languages.best_match(app.config['LANGUAGES'])
+    return best_match
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
