4faa0544ebb8f1767a1b55f421a373e419b77b9a
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-07-02 13:45:46 +0000

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -27,7 +27,7 @@ def index() -> str:
     """
     This is the home route.
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 @babel.localeselector
@@ -38,8 +38,11 @@ def get_locale() -> str:
     returns:
             str = best match.
     """
-    best_match = request.accept_languages.best_match(app.config['LANGUAGES'])
-    return best_match
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 
 if __name__ == '__main__':
